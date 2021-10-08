b9632719cf828eb217babc4083d1e406e7befc23
DiegoAMS <diego.slijkhuis@mansystems.nl>
2021-10-08 13:15:27 +0200

    Formatting

--- src/JavascriptButton/widget/JavaScriptButton.js
+++ src/JavascriptButton/widget/JavaScriptButton.js
@@ -27,8 +27,8 @@ define([
     "dojo/_base/lang",
     "dojo/text",
     "dojo/_base/event",
-	"dijit/registry",
-	"dojo/query",
+    "dijit/registry",
+    "dojo/query",
     "dojo/text!JavaScriptButton/widget/template/JavaScriptButton.html"
 ], function (declare, _WidgetBase, _TemplatedMixin, dojoClass, dojoConstruct, dojoLang, dojoText, dojoEvent, dojoRegistry, dojoQuery, widgetTemplate) {
     "use strict";
@@ -43,28 +43,28 @@ define([
 
         // Parameters configured in the Modeler.
         contenttype: "",
-		jsToExecute: "",
-		jsToExecuteAfter: "",
-		mfToExecute: "",
-		buttonClass: "",
-		buttonText:	 "",
-		confirmation: false,
-		confirmQuestion: "",
-		okButton: "",
-		cancelButton: "",
-		buttonTabIndex: 0,
+        jsToExecute: "",
+        jsToExecuteAfter: "",
+        mfToExecute: "",
+        buttonClass: "",
+        buttonText: "",
+        confirmation: false,
+        confirmQuestion: "",
+        okButton: "",
+        cancelButton: "",
+        buttonTabIndex: 0,
 
-		//Internal variables
-		_contextObj: null,
-		_listener: null,
+        //Internal variables
+        _contextObj: null,
+        _listener: null,
 
         // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
         update: function (obj, callback) {
             logger.debug(this.id + ".update");
 
-			this._contextObj = obj;
+            this._contextObj = obj;
             this._setupEvents();
-			this._updateRendering();
+            this._updateRendering();
 
             callback();
         },
@@ -72,96 +72,96 @@ define([
         // Rerender the interface.
         _updateRendering: function () {
             logger.debug(this.id + "._updateRendering");
-			this.theButton.innerHTML = this.buttonText;
+            this.theButton.innerHTML = this.buttonText;
 
-			this.theButton.setAttribute("tabIndex", this.buttonTabIndex);
+            this.theButton.setAttribute("tabIndex", this.buttonTabIndex);
 
-			if (this.buttonClass !== "") {
-				dojoClass.add(this.theButton, this.buttonClass);
-			}
+            if (this.buttonClass !== "") {
+                dojoClass.add(this.theButton, this.buttonClass);
+            }
         },
 
-		_setupEvents: function () {
-			if (this._listener) {
-				this.disconnect(this._listener);
-			}
+        _setupEvents: function () {
+            if (this._listener) {
+                this.disconnect(this._listener);
+            }
 
-			if (this.confirmation) {
-				if (this.jsToExecute) {
-					this._listener = this.connect(this.theButton, "click", this._confirm(this._executeJSBefore));
-				} else if (this.mfToExecute) {
-					this._listener = this.connect(this.theButton, "click", this._confirm(this._executeMicroflow));
-				}
-			} else {
-				if (this.jsToExecute) {
-					this._listener = this.connect(this.theButton, "click", this._executeJSBefore);
-				} else if (this.mfToExecute) {
-					this._listener = this.connect(this.theButton, "click", this._executeMicroflow);
-				}
-			}
-		},
+            if (this.confirmation) {
+                if (this.jsToExecute) {
+                    this._listener = this.connect(this.theButton, "click", this._confirm(this._executeJSBefore));
+                } else if (this.mfToExecute) {
+                    this._listener = this.connect(this.theButton, "click", this._confirm(this._executeMicroflow));
+                }
+            } else {
+                if (this.jsToExecute) {
+                    this._listener = this.connect(this.theButton, "click", this._executeJSBefore);
+                } else if (this.mfToExecute) {
+                    this._listener = this.connect(this.theButton, "click", this._executeMicroflow);
+                }
+            }
+        },
 
-		_confirm: function (callback) {
-			return function () {
-				mx.ui.confirmation({
-					content: this.confirmQuestion,
-					proceed: this.okButton,
-					cancel: this.cancelButton,
-					handler: dojoLang.hitch(this, callback)
-				});
-			};
-		},
+        _confirm: function (callback) {
+            return function () {
+                mx.ui.confirmation({
+                    content: this.confirmQuestion,
+                    proceed: this.okButton,
+                    cancel: this.cancelButton,
+                    handler: dojoLang.hitch(this, callback)
+                });
+            };
+        },
 
-		_executeMicroflow: function () {
-			if (this.mfToExecute) {
-				mx.ui.action(this.mfToExecute, {
-					progress: "nonmodal",
-					progressMsg: "",
-					params: {
-						applyto: "selection",
-						//actionname: this.mfToExecute,
-						guids: [ this._contextObj.getGuid() ]
-					},
-					origin: this.mxform,
-					callback: function (obj) {
-						if (this.jsToExecuteAfter) {
-							this._executeJSAfter();
-						}
-					}.bind(this),
-					error: dojoLang.hitch(this, function (error) {
-						logger.error(this.id + ": An error occurred while executing microflow: " + error.description);
-					})
-				}, this);
-			}
-		},
+        _executeMicroflow: function () {
+            if (this.mfToExecute) {
+                mx.ui.action(this.mfToExecute, {
+                    progress: "nonmodal",
+                    progressMsg: "",
+                    params: {
+                        applyto: "selection",
+                        //actionname: this.mfToExecute,
+                        guids: [ this._contextObj.getGuid() ]
+                    },
+                    origin: this.mxform,
+                    callback: function (obj) {
+                        if (this.jsToExecuteAfter) {
+                            this._executeJSAfter();
+                        }
+                    }.bind(this),
+                    error: dojoLang.hitch(this, function (error) {
+                        logger.error(this.id + ": An error occurred while executing microflow: " + error.description);
+                    })
+                }, this);
+            }
+        },
 
-		_executeJSBefore: function() {
-			this._executeJS(this.jsToExecute);
-			if (this.mfToExecute) {
-				this._executeMicroflow();
-			}
-		},
-		_executeJSAfter: function() {
-			this._executeJS(this.jsToExecuteAfter);
-		},
-		_executeJS: function(js) {
-			if (this.contenttype == "jsjQuery") {
-				require(["JavaScriptButton/lib/jquery-1.11.2"], this._evalJS.bind(this,js));
-			} else {
-				this._evalJS(js);
-			}
-		},
+        _executeJSBefore: function() {
+            this._executeJS(this.jsToExecute);
+            if (this.mfToExecute) {
+                this._executeMicroflow();
+            }
+        },
 
-		_evalJS: function (js) {
-			try {
-				eval(js + "\r\n//# sourceURL=" + this.id + ".js");
-			} catch (e) {
-				dojoConstruct.place("<div class=\"alert alert-danger\">Error while evaluating javascript input: " + e + "</div>", this.domNode, "only");
-			}
-		}
+        _executeJSAfter: function() {
+            this._executeJS(this.jsToExecuteAfter);
+        },
 
+        _executeJS: function(js) {
+            if (this.contenttype == "jsjQuery") {
+                require(["JavaScriptButton/lib/jquery-1.11.2"], this._evalJS.bind(this, js));
+            } else {
+                this._evalJS(js);
+            }
+        },
 
-	});
+        _evalJS: function (js) {
+            try {
+                eval(js + "\r\n//# sourceURL=" + this.id + ".js");
+            } catch (e) {
+                dojoConstruct.place("<div class=\"alert alert-danger\">Error while evaluating javascript input: " + e + "</div>", this.domNode, "only");
+            }
+        }
+    });
 });
 
 require(["JavaScriptButton/widget/JavaScriptButton"], function () {
